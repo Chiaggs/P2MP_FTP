98a0817279ed468e851515a7c171a9acb87d0386
Chiaggs <chiaggs@gmail.com>
2019-10-25 17:05:12 -0400

    Added code for file handling

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,10 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Client {
-    public static void main(String args[]){
+    public static final int SERVER_PORT = 7735;
+    public static void main(String args[]) throws Exception{
         int argLength = args.length;
         if(argLength > 2) {
             // Commandline Parameters
@@ -16,9 +21,20 @@ public class Client {
             for (int i = 0; i < argLength - 2; i++){
                 System.out.println("Servers: " + servers[i]);
             }
+
+            // Fetching the file
+            File temp = new File("src/"+filename);
+            if(temp.exists()) {
+                BufferedReader br1 = new BufferedReader(new FileReader(temp));
+                System.out.println("File exists, and added to buffered reader");
+            }
         }
         else{
             System.out.println("Input parameters incorrect, exiting.. ");
         }
+    }
+
+    public static void rdt_send(byte[] data){
+
     }
 }
