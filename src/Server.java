4f634eb8df28641319764174a763d36dfcd0ece5
Chiaggs <chiaggs@gmail.com>
2019-10-29 21:39:43 -0400

    Bugfixes

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.nio.ByteBuffer;
@@ -11,12 +10,8 @@ import java.util.Random;
 public class Server {
 
     public static final int PORT = 7735;
-    public static int BUFFER_SIZE = 65535;
     public static byte[] buffer;
     public static DatagramSocket UDP_Socket;
-    public static final int HEADER_SIZE = 8;
-    public static int ACK_1 = 0x0000;
-    public static int ACK_2 = 0xAAAA;
     public static double probabilistic_loss;
     public static ArrayList<Integer> listSeqNum;
     public static int oldSequenceNum;
@@ -24,7 +19,7 @@ public class Server {
 
     public static void Initialize_Server() throws Exception{
         UDP_Socket = new DatagramSocket(PORT);
-        buffer = new byte[BUFFER_SIZE];
+        buffer = new byte[65535];
         listSeqNum = new ArrayList<>();
     }
 
@@ -36,31 +31,26 @@ public class Server {
             while(true){
                 DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length);
                 UDP_Socket.receive(receivedPacket);
-                //System.out.println("Received data is: " + ConvertDataToString(buffer));
-                buffer = new byte[BUFFER_SIZE];
-
+                buffer = new byte[65535];
                 System.out.print("Server waiting for receiving files");
-
-                int seqNum = processRequest(Arrays.copyOfRange(receivedPacket.getData(), 0, receivedPacket.getLength()));
+                int seqNum = requestProcessor(Arrays.copyOfRange(receivedPacket.getData(), 0, receivedPacket.getLength()));
                 System.out.println("SeqNum is: " + seqNum);
-
-                int size = ((Integer.SIZE)/8) + ((Character.SIZE * 2)/8);
-                System.out.println("Size is: " + size);
-                ByteBuffer bf = ByteBuffer.allocate(size);
-                bf.putInt(seqNum);
-                bf.putChar((char)ACK_1);
-                bf.putChar((char)ACK_2);
-                byte[] segmentAck = bf.array();
-
-                receivedPacket.setData(segmentAck);
-                UDP_Socket.send(receivedPacket);
-                buffer = new byte[BUFFER_SIZE];
+                StringBuilder convertedData = ConvertDataToString(receivedPacket.getData());
+                if(convertedData != null){
+                    ByteBuffer bf = ByteBuffer.allocate(8);
+                    bf.putInt(seqNum);
+                    bf.putChar((char)0x0000);
+                    bf.putChar((char)0xAAAA);
+                    byte[] segmentAck = bf.array();
+                    receivedPacket.setData(segmentAck);
+                    UDP_Socket.send(receivedPacket);
+                    buffer = new byte[65535];
+                }
             }
         }
         else{
             System.out.println("Input parameters incorrect, exiting.. ");
         }
-
     }
 
     public static StringBuilder ConvertDataToString(byte[] a)
@@ -77,62 +67,47 @@ public class Server {
         return ret;
     }
 
-    public static int processRequest(byte[] data) {
-        byte[] sequenceBytes = Arrays.copyOfRange(data, 0, 4);
-        byte[] checksumBytes = new byte[4];
-        byte[] _checksumBytes = Arrays.copyOfRange(data, 4, 6);
-        checksumBytes[0] = 0;
-        checksumBytes[1] = 0;
-        checksumBytes[2] = _checksumBytes[0];
-        checksumBytes[3] = _checksumBytes[1];
-
-        byte[] dataBytes = Arrays.copyOfRange(data, HEADER_SIZE, data.length);
-
-        int sequenceNum = ByteBuffer.wrap(sequenceBytes).getInt();
-        int mss = data.length - HEADER_SIZE;
-
+    public static int requestProcessor(byte[] data) {
+        byte[] checksumProcessorByteArray = new byte[4];
+        byte[] hashedProcessorHelper = Arrays.copyOfRange(data, 4, 6);
+        for(int i = 0; i < 4; i ++ ){
+            if(i == 0 || i == 1)
+                checksumProcessorByteArray[i] = 0;
+            else
+                checksumProcessorByteArray[i] = hashedProcessorHelper[i-2];
+        }
+        int sequenceNum = ByteBuffer.wrap(Arrays.copyOfRange(data, 0, 4)).getInt();
         if(!checkProbValue(sequenceNum)) {
             return -1;
         }
-
-        int checksum = ByteBuffer.wrap(checksumBytes).getInt();
-        if(!checkCheckSum(dataBytes, checksum)) {
+        int checksum = ByteBuffer.wrap(checksumProcessorByteArray).getInt();
+        if(!checkCheckSum(Arrays.copyOfRange(data, 8, data.length), checksum)) {
             return -1;
         }
-
-        int ackSequenceNum = getSequenceNumber(sequenceNum, mss);
+        int ackSequenceNum = getSequenceNumber(sequenceNum, data.length - 8);
         System.out.println("\nAck num: " + ackSequenceNum);
-
         if (ackSequenceNum != -1) {
-            saveToFile(dataBytes);
+            saveToFile(Arrays.copyOfRange(data, 8, data.length));
         }
-
         oldSequenceNum = sequenceNum;
-        //oldMSS = mss;
-
         return ackSequenceNum;
     }
 
     public static Boolean checkProbValue(int sequenceNum) {
         double r = new Random().nextDouble();
+        boolean shouldReturn = true;
         if(r <= probabilistic_loss) {
             System.out.print("\nPacket discarded hence packet loss where sequence number = "+sequenceNum);
-            return false;
+            shouldReturn = false;
         }
-        return true;
+        return shouldReturn;
     }
 
     public static Boolean checkCheckSum(byte[] data, int checksum) {
-
         ByteBuffer ba = ByteBuffer.allocate(data.length + (Integer.SIZE)/8);
         ba.putInt(checksum);
         ba.put(data);
-        byte [] input = ba.array();
-
-        long dataSum = generateSumofDataandCheckum(input);
-        boolean isVerified = (dataSum == 0xFFFF);
-
-        return isVerified;
+        return (generateSumofDataandCheckum(ba.array()) == 0xFFFF);
     }
 
     public static long generateSumofDataandCheckum(byte[] input) {
@@ -162,24 +137,19 @@ public class Server {
     }
 
     public static int getSequenceNumber(int seqNum, int mss) {
-        int temp = seqNum + mss;
         int size = listSeqNum.size();
-
         if (size == 0) {
-            listSeqNum.add(temp);
-            return temp;
+            listSeqNum.add(seqNum + mss);
+            return seqNum + mss;
         }
-
         int prevSeqNum = listSeqNum.get(size - 1);
-
         if(seqNum == oldSequenceNum) {
             System.out.print("\nDuplicate Seq # " + seqNum + " received. " + "Resending previous ACK number # " + prevSeqNum);
             return prevSeqNum;
         }
-
-        if((prevSeqNum + mss) == temp) {
-            listSeqNum.add(temp);
-            return temp;
+        else if((prevSeqNum + mss) == seqNum + mss) {
+            listSeqNum.add(seqNum + mss);
+            return seqNum + mss;
         }
         else {
             System.out.print("\nSequence number from client # " + seqNum + " is wrong. " + "Requiring # " + prevSeqNum );
